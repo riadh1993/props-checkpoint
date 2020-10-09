808dae6f746749c5e0f41ad9d1513387f1881799
riadh1993 <riadhkar1993@gmail.com>
2020-10-09 13:20:51 +0100

    first commit

---
+++ react-training/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Profile from "./profile/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-training/src/profile/Profile.js
@@ -0,0 +1,64 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+
+ 
+const Profile = () => {
+  return (
+    <div style={{fontSize: "30px",fontWeight: "bold",margin: "30px",color: "purple"}}>
+    <FullName ></FullName>
+    <Biography></Biography>
+    <Profession></Profession>
+    <Alert></Alert>
+
+    </div>
+  )
+}
+  const FullName = props => {
+    return (
+      <div>
+        <h1>FullName: {props.FullName} </h1>
+      </div>
+    )
+  }
+  const Biography = props => {
+    return (
+      <div>
+        <h1>Biography: {props.Biography} </h1>
+      </div>
+    )
+  }
+  const Profession = props => {
+    return (
+      <div>
+        <h1>Profession: {props.Profession} </h1>
+      </div>
+    )
+  }
+  const Alert = (props) => {
+     return(
+       <h1>Alert: {props.Alert} </h1>
+     )
+  }
+  
+  FullName.defaultProps = {
+    FullName : "John Wick"
+  };
+  
+  Biography.defaultProps = {
+    Biography : "Born in 1973 and still killing people for money"
+  }
+  Profession.defaultProps = {
+    Profession : "An assasin"
+  };
+  Alert.defaultProps = {
+    Alert: "Fullname is John Wick"
+  }
+
+ Biography.protoTypes = {
+  booleanProp: PropTypes.string,
+ }
+
+
+export default Profile;
